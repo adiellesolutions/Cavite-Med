bb5fd32b768cb5112393d1fb9a16d32c1cbb8d7a
Adielle Solutions <adielle.solutions@gmail.com>
2026-02-20 12:31:57 +0800

    db Local

--- backend/db/cavitemed_db.php
+++ backend/db/cavitemed_db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "sql101.infinityfree.com";
-$user = "if0_40076005";          // change if needed
-$pass = "dvh6NY9mMN7";              // change if needed
-$db   = "if0_40076005_cavitemed";
+// Database connection
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'cavitemed';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
